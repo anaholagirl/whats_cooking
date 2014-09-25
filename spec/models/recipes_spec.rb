bc87c49d44ba157b57846a21a6eae901c330443c
Linda Gibson <dakidz@live.com>
2014-09-25 10:35:01 -0700

    validate presence of recipe instructions

--- spec/models/recipes_spec.rb
+++ spec/models/recipes_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
   describe Recipe do
     it { should validate_presence_of :name }
     it { should validate_presence_of :ingredients }
+    it { should validate_presence_of :instructions }
 
   end
