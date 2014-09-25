8fb945fc7a3c65a5e68f6c353d3c1d45ec7181b2
Linda Gibson <dakidz@live.com>
2014-09-25 09:28:38 -0700

    add favorites table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925162534) do
+ActiveRecord::Schema.define(version: 20140925162739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: true do |t|
+    t.integer "recipe_id"
+    t.integer "user_id"
+  end
 
   create_table "recipes", force: true do |t|
     t.string "name"
