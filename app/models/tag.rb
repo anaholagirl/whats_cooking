212623c072c617b454d9d9b340e1fa6fddbf57a5
Linda Gibson <dakidz@live.com>
2014-09-25 13:18:38 -0700

    establish association tag belongs to a recipe

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,6 @@ class Tag < ActiveRecord::Base
 
   validates :recipe_id, presence: true
   validates :type_id, presence: true
+
+  belongs_to :recipe
 end
--- spec/models/tags_spec.rb
+++ spec/models/tags_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
   describe Tag do
     it { should validate_presence_of :recipe_id }
     it { should validate_presence_of :type_id }
+
+    it { should belong_to :recipe }
   end
