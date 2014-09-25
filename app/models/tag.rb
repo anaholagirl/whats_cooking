aee80068ece62f3b9845dae58467ae93459a6522
Linda Gibson <dakidz@live.com>
2014-09-25 10:42:38 -0700

    validate presence of type_id for tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ActiveRecord::Base
 
   validates :recipe_id, presence: true
+  validates :type_id, presence: true
 end
--- spec/models/tags_spec.rb
+++ spec/models/tags_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
   describe Tag do
     it { should validate_presence_of :recipe_id }
+    it { should validate_presence_of :type_id }
   end
