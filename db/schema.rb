dabe14f4735be2283c526e6a1d313a1f40433bcb
Linda Gibson <dakidz@live.com>
2014-09-25 09:31:25 -0700

    add photos table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925162739) do
+ActiveRecord::Schema.define(version: 20140925162901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "favorites", force: true do |t|
+    t.integer "recipe_id"
+    t.integer "user_id"
+  end
+
+  create_table "photos", force: true do |t|
+    t.string  "name"
     t.integer "recipe_id"
     t.integer "user_id"
   end
