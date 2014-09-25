90a7d0163287c2efbebb4d2dcb36f0c46400ee63
Linda Gibson <dakidz@live.com>
2014-09-25 09:25:06 -0700

    add types table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925161944) do
+ActiveRecord::Schema.define(version: 20140925162333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20140925161944) do
     t.string "name"
     t.text   "ingredients"
     t.text   "instructions"
+  end
+
+  create_table "types", force: true do |t|
+    t.string "name"
   end
 
   create_table "users", force: true do |t|
