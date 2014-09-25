85c80f5435956c3fbcd79df046f507c720d599c3
Linda Gibson <dakidz@live.com>
2014-09-25 10:33:59 -0700

    validate presence of recipe ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ActiveRecord::Base
 
   validates :name, presence: true
+  validates :ingredients, presence: true
 end
--- spec/models/recipes_spec.rb
+++ spec/models/recipes_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
   describe Recipe do
     it { should validate_presence_of :name }
+    it { should validate_presence_of :ingredients }
 
   end
