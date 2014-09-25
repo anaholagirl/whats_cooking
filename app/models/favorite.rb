3f93135f35eda421b5cbf7dd341281ef136d4a41
Linda Gibson <dakidz@live.com>
2014-09-25 13:21:14 -0700

    establish association favorite belongs to a user

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,4 +2,6 @@ class Favorite < ActiveRecord::Base
 
   validates :recipe_id, presence: true
   validates :user_id, presence: true
+
+  belongs_to :user
 end
--- spec/models/favorites_spec.rb
+++ spec/models/favorites_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
   describe Favorite do
     it { should validate_presence_of :recipe_id }
     it { should validate_presence_of :user_id }
+
+    it { should belong_to :user }
   end
