1fbd77d4cd30d423eeabbecc4db1b510d310b9e7
Linda Gibson <dakidz@live.com>
2014-09-25 13:38:00 -0700

    add user_id column to tags table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925175101) do
+ActiveRecord::Schema.define(version: 20140925203642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20140925175101) do
   create_table "tags", force: true do |t|
     t.integer "type_id"
     t.integer "recipe_id"
+    t.integer "user_id"
   end
 
   create_table "types", force: true do |t|
