e9b0411cd1e694d3058d513616676e8da4f73c60
Linda Gibson <dakidz@live.com>
2014-09-25 13:15:40 -0700

    establish association photo belongs to user

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,4 +3,6 @@ class Photo < ActiveRecord::Base
   validates :name, presence: true
   validates :recipe_id, presence: true
   validates :user_id, presence: true
+
+  belongs_to :user
 end
--- spec/models/photos_spec.rb
+++ spec/models/photos_spec.rb
@@ -4,4 +4,6 @@ require 'rails_helper'
     it { should validate_presence_of :name }
     it { should validate_presence_of :recipe_id }
     it { should validate_presence_of :user_id }
+
+    it { should belong_to :user }
   end
