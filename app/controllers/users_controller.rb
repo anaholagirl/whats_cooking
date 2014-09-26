338e1d28d30b8d22fffabbc09627d037294b7823
Linda Gibson <dakidz@live.com>
2014-09-25 17:38:27 -0700

    allow a new user to sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
 class UsersController < ApplicationController
 
+   before_action :authorize, only: :destroy
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = 'Account successfully created'
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:alert] = 'Account deleted'
+    redirect_to root_path
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name :email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  root to: 'recipes#index'
 
   resources :recipes
   resources :users
