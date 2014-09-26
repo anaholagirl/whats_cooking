3b82914130da5817ab46ed1b80922e61d204dac2
Linda Gibson <dakidz@live.com>
2014-09-26 15:43:23 -0700

    complete tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,55 @@
 class TagsController < ApplicationController
 
+  def index
+    @tags = Tag.all
+  end
 
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @tag = @user.tags.new(tag_params)
+    if @tag.save
+      flash[:notice] = "You have successfully tagged this recipe!"
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @tag = Tag.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @tag = Tag.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @tag = Tag.find(params[:id])
+    if @tag.update(tag_params)
+      flash[:notice] = "Your tag has been updated!"
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+      flash[:notice] = "Tag was deleted!"
+      redirect_to user_path(@user)
+  end
+
+private
+  def tag_params
+    params.require(:tag).permit(:recipe_id, :group_id, :user_id).merge(:user_id => current_user.id)
+  end
 end
