d0c6c406f87c0362c7cc6d6628c5c3f7b989b6b6
Linda Gibson <dakidz@live.com>
2014-09-28 16:42:13 -0700

    test for errors in signup process

--- spec/features/signup_pages_spec.rb
+++ spec/features/signup_pages_spec.rb
@@ -10,4 +10,30 @@ describe "signing up" do
     click_button 'Create User'
     expect(page).to have_content 'Welcome to'
   end
+
+  it "show an error with a blank name field." do
+    visit 'signup'
+    fill_in 'Email', :with => 'user@example.com'
+    fill_in 'Password', :with => 'password'
+    fill_in 'Password confirmation', :with => 'password'
+    click_button 'Create User'
+    expect(page).to have_content "can't be blank"
+  end
+
+  it "show an error with a blank password and password confirmation field." do
+    visit 'signup'
+    fill_in 'Name', :with => 'username'
+    fill_in 'Email', :with => 'user@example.com'
+    click_button 'Create User'
+    expect(page).to have_content "can't be blank"
+  end
+
+  it "show an error with a blank email field, but correct password and password confirmation field." do
+    visit 'signup'
+    fill_in 'Name', :with => 'username'
+    fill_in 'Password', :with => 'password'
+    fill_in 'Password confirmation', :with => 'password'
+    click_button 'Create User'
+    expect(page).to have_content "can't be blank"
+  end
 end
