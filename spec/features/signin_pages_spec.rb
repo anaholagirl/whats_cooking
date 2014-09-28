96dd30a6478d7e03f020edbec7c9c0bb3a87b18c
Linda Gibson <dakidz@live.com>
2014-09-28 16:49:41 -0700

    test for errors in signin process

--- spec/features/signin_pages_spec.rb
+++ spec/features/signin_pages_spec.rb
@@ -9,4 +9,27 @@ describe "signing in" do
     click_button 'Log In'
     expect(page).to have_content 'Logged in'
   end
+
+  it "shows error with wrong password" do
+    visit '/sessions/new'
+    user = User.create(:email => 'user@example.com', :password => 'password', :password_confirmation => 'password')
+    fill_in 'Email', :with => 'user@example.com'
+    fill_in 'Password', :with => 'wrong'
+    click_button 'Log In'
+    expect(page).to have_content 'invalid'
+  end
+
+  it "show an error with a blank password field." do
+    visit 'login'
+    fill_in 'Email', :with => 'user@example.com'
+    click_button 'Log In'
+    expect(page).to have_content 'invalid'
+  end
+
+  it "show an error with a blank email field." do
+    visit 'login'
+    fill_in 'Password', :with => 'password'
+    click_button 'Log In'
+    expect(page).to have_content 'invalid'
+  end
 end
