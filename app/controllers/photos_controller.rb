aee0dcf2d442f90895927598040d0afb8b93cd6c
Linda Gibson <dakidz@live.com>
2014-10-04 18:26:48 -0700

    add columns to photos table for images

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,24 @@
 class PhotosController < ApplicationController
 
+  def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @photo = Photo.new
+  end
 
+  def create
+    @user = User.find(session[:user_id])
+    @recipe = Recipe.find(params[:recipe_id])
+    @photo = @recipe.photos.create(photo_params)
+    if @photo.valid?
+      redirect_to recipe_path(@recipe)
+    else
+      render 'new'
+    end
+  end
+
+private
+  def photo_params
+    params.require(:photo).permit(:name, :user_id, :recipe_id, :image).merge(:name => @recipe.name, :user_id => current_user.id)
+  end
 end
+
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,10 @@ class Photo < ActiveRecord::Base
   validates :name, presence: true
   validates :recipe_id, presence: true
   validates :user_id, presence: true
-
+  # has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  # validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :photo, :styles => { :large => "700x700", :medium => "500x500", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
   belongs_to :user
   belongs_to :recipe
 end
