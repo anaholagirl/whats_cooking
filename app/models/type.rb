161bbf6c5d84d16d1efd1e648657bfd0f3bff4d5
Linda Gibson <dakidz@live.com>
2014-10-21 20:42:51 -0700

    whats-cooking-101

--- app/models/type.rb
+++
@@ -1,6 +0,0 @@
-class Type < ActiveRecord::Base
-
-  validates :name, presence: true
-
-  has_many :tags
-end
--- spec/models/types_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-  describe Type do
-    it { should validate_presence_of :name }
-
-    it { should have_many :tags }
-  end
